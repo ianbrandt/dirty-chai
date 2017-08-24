4c40754e5fc5a95d938263e325b1e00c1c95766e
Ian Brandt <ian@ianbrandt.com>
2017-08-24 01:01:18 -0700

    Improved TODOs.

--- lib/dirty-chai.d.ts
+++ lib/dirty-chai.d.ts
@@ -4,11 +4,13 @@ declare global {
   namespace Chai {
     interface Assertion extends LanguageChains, NumericComparison, TypeComparison {
 
-      // TODO: This would work if defined in the base Assertion declaration
-      // Otherwise... TS2403: Subsequent variable declarations must have the same type.
+      // TODO: Union type results in "TS2403: Subsequent variable declarations must have the same type."
+      // Or, if in the base Chai declaration, at the call site it results in "TS2349: Cannot invoke an
+      // expression whose type lacks a call signature. Type 'Assertion | ((message?: string) => Assertion)'
+      // has no compatible call signatures."
       // ok: Assertion | ((message?: string) => Assertion);
 
-      // TODO: TS2300: Duplicate identifier
+      // TODO: Function type results in "TS2300: Duplicate identifier."
       // ok(message?: string): Assertion;
       // true(message?: string): Assertion;
       // false(message?: string): Assertion;
@@ -23,7 +25,7 @@ declare global {
       // sealed(message?: string): Assertion;
       // frozen(message?: string): Assertion;
 
-      // This works because finite() is missing from the current version of the base Chai.Assertion type definition
+      // This works because finite() is missing from the current version of the base Chai.Assertion type declaration
       finite(message?: string): Assertion;
     }
   }
